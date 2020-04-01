0e75ac610fdeb281ec90e0c0779f1435b373d203
ouersl <2569792062@qq.com>
2020-04-01 17:19:26 +0800

    增加模板

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 app = Flask(__name__)
 
 @app.route('/')
-def idnex():
-    return '<h1>demo..</h1>'
+def index():
+    name='王涵'
+    movies=[
+        {'title':'王涵','year':20}
+
+    ]
+    return render_template('index.html',name=name,movies=movies)
+
+@app.route('/index/<conn>')
+def con(conn):
+    print(url_for('index',n=conn))
+
+    return 'hello %s'%conn
